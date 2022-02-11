083e458fad2435f2adce6b756bf77405c5a1cd9f
Ayron's Macbook <ayayron@Ayrons-MacBook-Pro.local>
2022-02-11 17:33:05 -0500

    added snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Renderer from 'react-test-renderer';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('rednering snapshot', () => {
+  const testRenderer = Renderer.create(<App />).toJSON
+  expect(testRenderer).toMatchSnapshot()
+})
