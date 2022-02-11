f07f3f06e6ba477ec8df5d52162aecc1b2def6f8
Ayron's Macbook <ayayron@Ayrons-MacBook-Pro.local>
2022-02-11 18:29:00 -0500

    gitCard now displays github name

--- src/components/gitCard/index.js
+++ src/components/gitCard/index.js
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react"
+
 function GitCard() {
+    const [githubName, setgithubName] = useState()
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/dnsghd49')
+          .then(res => res.json())
+          .then(data => {
+            setgithubName(data.name)
+            console.log(data)
+          })
+      }, [])
+
     return (
         <div>
-            <h2>test</h2>
+            <h2>{githubName}</h2>
         </div>
     )
 }
